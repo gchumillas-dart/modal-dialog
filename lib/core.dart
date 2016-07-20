51d44ae64dce325b18dd5e7a709da0267d7d95f4
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-20 22:13:10 +0200

    add source files

---
+++ lib/core.dart
@@ -0,0 +1,6 @@
+library modal_dialog;
+
+import 'package:bootjack/bootjack.dart';
+import 'package:domelement/core.dart';
+
+part 'modal_message.dart';
---
+++ lib/modal_message.dart
@@ -0,0 +1,35 @@
+part of modal_dialog;
+
+class ModalMessage {
+  final String _title;
+  final String _message;
+
+  ModalMessage(this._title, this._message) {
+    DomElement md = $('<div class="modal" tabindex="-1" role="dialog" />')
+      ..append((DomElement target) {
+        target
+          ..appendElement($('<div class="modal-dialog" />')
+            ..append((DomElement target) {
+              target
+                ..appendElement($('<div class="modal-content" />')
+                  ..appendElement($('<div class="modal-header" />')
+                    ..appendElement($('<button type="button" class="close">')
+                      ..data["dismiss"] = "modal"
+                      ..html = '&times;')
+                    ..appendElement($('<h3 />')..text = _title))
+                  ..appendElement($('<div class="modal-body" />')
+                    ..appendElement($('<p />')..text = _message))
+                  ..appendElement($('<div class="modal-footer" />')
+                    ..appendElement($('<button class="btn btn-default">')
+                      ..data['dismiss'] = 'modal'
+                      ..text = 'Close')
+                    ..appendElement(
+                        $('<button class="btn btn-primary">')..text = 'Save')));
+            }));
+      })
+      ..appendTo(find('body'));
+
+    Modal m = new Modal(md.nativeElement);
+    m.toggle();
+  }
+}
