778fe4d7775099790ae6d73ad96cbdd786945cdb
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-21 20:44:53 +0200

    modal alert

--- lib/core.dart
+++ lib/core.dart
@@ -3,4 +3,5 @@ library modal_dialog;
 import 'package:bootjack/bootjack.dart';
 import 'package:domelement/core.dart';
 
+part 'modal_alert.dart';
 part 'modal_message.dart';
---
+++ lib/modal_alert.dart
@@ -0,0 +1,19 @@
+part of modal_dialog;
+
+class ModalAlert {
+  ModalMessage _modal;
+  DomElement _acceptButton;
+
+  ModalAlert(String title, String message) {
+    _modal = new ModalMessage(title, message);
+    _acceptButton = _modal.addButton('Accept', type: 'primary');
+  }
+
+  void onAccept(Function listener) {
+    _acceptButton.on('click', listener);
+  }
+
+  void close() {
+    _modal.close();
+  }
+}
