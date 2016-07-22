dea6c78f768e03eb64bccbfdc98b9a898d306273
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-22 20:41:03 +0200

    sort members

--- lib/modal_alert.dart
+++ lib/modal_alert.dart
@@ -4,12 +4,12 @@ class ModalAlert extends ModalDialog {
   ModalMessage _modalMessage;
   DomElement _acceptButton;
 
-  Modal get modal => _modalMessage.modal;
-
   ModalAlert(String title, {String text, String html}) {
     _modalMessage = new ModalMessage(title, text: text, html: html);
     _acceptButton = _modalMessage.addButton('Accept', type: 'primary');
   }
+
+  Modal get modal => _modalMessage.modal;
 
   void onAccept(Function listener) => _acceptButton.on('click', listener);
 }
