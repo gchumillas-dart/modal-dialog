38cd1a10536eda8adc0d8afacbe7da11bc130566
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-24 22:24:56 +0200

    named parameters: accept, cancel

--- lib/modal_confirm.dart
+++ lib/modal_confirm.dart
@@ -2,18 +2,21 @@ part of modal_dialog;
 
 class ModalConfirm extends ModalDialog {
   ModalMessage _modalMessage;
-  DomElement _acceptButton;
 
   /// Creates a modal confirm dialog with a [text] and a message.
   ///
   /// The message can be either a plain [text] or a [html] text.
-  ModalConfirm(String title, {String text, String html}) {
+  ModalConfirm(String title,
+      {String text,
+      String html,
+      ActionCallback cancel: defaultAction,
+      ActionCallback accept: defaultAction}) {
     _modalMessage = new ModalMessage(title, text: text, html: html);
-    _modalMessage.addButton('No')..on('click', () => _modalMessage.close());
-    _acceptButton = _modalMessage.addButton('Yes', type: 'primary');
+
+    _modalMessage
+      ..addButton('No', action: cancel)
+      ..addButton('Yes', action: accept, type: 'primary');
   }
 
   Modal get modal => _modalMessage.modal;
-
-  void onAccept(Function listener) => _acceptButton.on('click', listener);
 }
