580c672db8eced0fa1b9d283608e98ded244186d
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-24 21:15:23 +0200

    sort members

--- lib/modal_dialog.dart
+++ lib/modal_dialog.dart
@@ -1,8 +1,8 @@
 part of modal_dialog;
 
-typedef void Callback(ModalDialog dialog);
-
 void defaultAction(ModalDialog dialog) => dialog.close();
+
+typedef void Callback(ModalDialog dialog);
 
 abstract class ModalDialog {
   Modal get modal;
