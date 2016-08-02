f53ead6ca600eed81fcf00a7314cf6d56a73e50d
soloproyectos <gonzalo.chumillas@gmail.com>
2016-08-02 21:44:54 +0200

    sort members

--- lib/modal_dialog.dart
+++ lib/modal_dialog.dart
@@ -1,13 +1,13 @@
 part of modal_dialog;
 
 abstract class ModalDialog {
-  /// Opens the modal dialog
-  Future<Modal> open();
-
   /// Closes the modal dialog
   Future<Null> close() async {
     Modal m = await open();
     m.hide();
     m.element.remove();
   }
+
+  /// Opens the modal dialog
+  Future<Modal> open();
 }
